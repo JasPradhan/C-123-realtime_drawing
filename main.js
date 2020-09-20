0b95c9ee832c0a45614302b7e7f1f316983d6959
JasPradhan <70465360+JasPradhan@users.noreply.github.com>
2020-09-20 12:40:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-         
+function preload(){
+
+}
+
+function setup(){
+video=createCapture(VIDEO);
+video.size(550,500);    
+canvas=createCanvas(550,500);
+canvas.position(700,150);
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses)
+}
+
+function modelLoaded(){
+    console.log("Pose Net is initialized");
+}
+
+function draw(){
+background('#0069d1');
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+    }
+}
